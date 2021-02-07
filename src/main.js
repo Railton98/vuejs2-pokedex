120a65618ec7eddcd9c545acbd5dc3c0c14ef915
Railton98 <railtonleal98@gmail.com>
2021-02-07 02:45:38 -0300

    css to production

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import '../node_modules/bulma/css/bulma.css'
-// import '../node_modules/bulma/css/bulma.min.css'
+import '../node_modules/bulma/css/bulma.min.css'
 
 Vue.config.productionTip = false
 

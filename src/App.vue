d1fc9302a6a3efe475e0120bd17709e508061a01
Railton98 <railtonleal98@gmail.com>
2021-02-07 02:42:01 -0300

    finally adjusts

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div id="app">
-    <div
-      class="column is-half is-offset-one-quarter"
-      v-for="(poke, index) in pokemons"
-      :key="index"
-    >
-      <Pokemon :num="index + 1" :name="poke.name" :url="poke.url" />
+    <div class="column is-half is-offset-one-quarter">
+      <input
+        class="input is-rounded"
+        type="text"
+        placeholder="Buscar pokemon pelo nome..."
+        v-model="search"
+      />
+      <button
+        id="searchBtn"
+        class="button is-fullwidth is-success"
+        @click.prevent="find"
+      >
+        Buscar
+      </button>
+      <div v-for="(poke, index) in filteredPokemons" :key="poke.url">
+        <Pokemon :num="index + 1" :name="poke.name" :url="poke.url" />
+      </div>
     </div>
   </div>
 </template>
@@ -22,15 +33,35 @@ export default {
   data() {
     return {
       pokemons: [],
+      filteredPokemons: [],
+      search: "",
     }
   },
   created() {
     axios
       .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
-      .then((res) => (this.pokemons = res.data.results))
+      .then((res) => {
+        this.pokemons = res.data.results
+        this.filteredPokemons = res.data.results
+      })
+  },
+  methods: {
+    find() {
+      this.filteredPokemons = this.pokemons
+      if (this.search == "" || this.search == " ") {
+        this.filteredPokemons = this.pokemons
+      } else {
+        this.filteredPokemons = this.pokemons.filter((pokemon) =>
+          pokemon.name.includes(this.search.toLocaleLowerCase())
+        )
+      }
+    },
   },
 }
 </script>
 
-<style>
+<style scoped>
+#searchBtn {
+  margin-top: 2%;
+}
 </style>
